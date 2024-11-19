7545354078e2ecaf6ff15a2dcee011c2f15e4ab1
Haris Riyoni <1214047@std.ulbi.ac.id>
2024-11-20 01:27:21 +0700

    Update Organisasis Relation in User

--- app/Models/Organisasi.php
+++ app/Models/Organisasi.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Organisasi extends Model
 {
@@ -11,4 +12,9 @@ class Organisasi extends Model
     protected $keyType = 'int';
     public $incrementing = true;
     public $timestamps = true;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
