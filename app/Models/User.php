6ba96a19572689379e944a1fc995b8188127d02a
Haris Riyoni <1214047@std.ulbi.ac.id>
2024-11-20 01:31:56 +0700

    Update Implements JWT

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -33,6 +34,14 @@ class User extends Authenticatable
         return [
             'password' => 'hashed',
         ];
+    }
+    public function getJWTIdentifier()
+    {
+        $this->getKey();
+    }
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
     public function organisasis(): HasMany
     {
